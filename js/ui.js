a8754f6e1c117429553a59cb3b2e436e6bdd9e10
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-09-18 11:32:05 +1200

    add about page

--- js/ui.js
+++ js/ui.js
@@ -14,7 +14,7 @@ export function displayVenues(list) {
         // If there is a link, show it; otherwise show logical text
         const linkHTML = v.link
             ? `<a href="${v.link}" target="_blank">View website</a>`
-            : `<p style="color:#888; font-style:italic;">No website available</p>`;
+            : `<p style="color:#888; font-style:italic;">No website available (if you've found one, <a href="https://docs.google.com/forms/d/e/1FAIpQLSeUL7smwdQPWHp1Xgl7rZnyWGPUghqX8f7n7u0uj4lg8JkvlA/viewform?usp=dialog" target="_blank">request a change</a>)</p>`;
 
         card.innerHTML = `
             <h2>${v.name}</h2>
