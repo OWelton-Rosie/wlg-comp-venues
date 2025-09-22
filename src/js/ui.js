5d6f1068eb71b910397f5c9fa2bbe5a92bbd968e
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-09-23 10:19:16 +1200

    updates to JSON and ui

--- src/js/ui.js
+++ src/js/ui.js
@@ -51,7 +51,7 @@ export function displayVenues(list) {
 
             const linkHTML = v.link
                 ? `<a href="${v.link}" target="_blank">View website</a>`
-                : `<p class="no-link">No website available (if you've found one, <a href="https://docs.google.com/forms/d/e/1FAIpQLSeUL7smwdQPWHp1Xgl7rZnyWGPUghqX8f7n7u0uj4lg8JkvlA/viewform?usp=dialog" target="_blank">request a change</a>)</p>`;
+                : `<i><p class="no-link">No website available (if you've found one, <a href="https://docs.google.com/forms/d/e/1FAIpQLSeUL7smwdQPWHp1Xgl7rZnyWGPUghqX8f7n7u0uj4lg8JkvlA/viewform?usp=dialog" target="_blank">request a change</a>)</p></i>`;
 
             card.innerHTML = `
                 <h2>${v.name}</h2>
